aecda233576a5fcc8ee124a013d55942dac6dbce
DCrowder <DCrowder@harding.edu>
2015-04-20 00:18:52 -0500

    Dysfunctional Code with Progress
    Numbers are detected for rows only.  Assignments are wrong and ASCII flags are printing to LCD incorrectly.  Rows and Columns do function properly as tested with IncompleteCheck.c code.

--- TestCode3Numpad.c
+++ TestCode3Numpad.c
@@ -1,15 +1,12 @@
+//      Author:     David Crowder
+//      Name:       TestCod3Numpad.c
+//      Purpose:    Checks the value of the number pad and displays the number
+//                  to the screen.
+
+//      MCU header info
 #include <PIC16f887.h>
-
-// PIC16F887 Configuration Bit Settings
-
-// 'C' source line config statements
-
 #include <htc.h>
-
-#include <pic.h> /* This header file contains symbolic register addresses
-that may be used to perform input/output operations
-and to access other resources on the PIC 16F887 microcontroller*/
-
+#include <pic.h>
 
 
 __CONFIG(FOSC_INTRC_NOCLKOUT & WDTE_OFF & PWRTE_ON & MCLRE_OFF & CP_OFF & CPD_OFF & BOREN_OFF & IESO_OFF & FCMEN_OFF & LVP_OFF);
@@ -50,10 +47,30 @@ __CONFIG(BOR4V_BOR21V & WRT_OFF);
 #define Row3_dir TRISB2
 #define Row4_dir TRISB3
 
+// Numpad Ports
+#define COL1 RE0
+#define COL2 RE1
+#define COL3 RE2
+
+#define ROW1 RB0
+#define ROW2 RB1
+#define ROW3 RB2
+#define ROW4 RB3
+
+// Direction of Numpad Ports
+#define DIRCOL1 TRISE0
+#define DIRCOL2 TRISE1
+#define DIRCOL3 TRISE2
+
+#define DIRROW1 TRISB0
+#define DIRROW2 TRISB1
+#define DIRROW3 TRISB2
+#define DIRROW4 TRISB3
+
 void delay(unsigned int time)
 {
     unsigned int i;
-    
+
    for(i = 0; i < time; i++); //Loop
 
 }
@@ -90,7 +107,7 @@ void InitLCD(void)
         LCD_RW               = 0;
 	LCD_RS    	     = 0;      // D = 0
 	LCD_DAT              = 0;      // CLK = 0
-        
+
 	LCD_E_Dir            = 0;      // Make Output
         LCD_RW_Dir           = 0;      // Make Output
         LCD_RS_Dir           = 0;      // Make Output
@@ -120,9 +137,6 @@ void InitLCD(void)
 
 }
 
-
-
-
 void WriteStringToLCD(const char *s)
 {
     while(*s)
@@ -131,12 +145,30 @@ void WriteStringToLCD(const char *s)
     }
 }
 
-
-
 void ClearLCDScreen(void)
 {
 	WriteCommandToLCD(0x01);    // Clear the screen
 	delay(35000);              // Delay for cursor to return at zero position
+}
+
+char * IntToText(int n)
+{
+  static int bin[5];
+  bin[3] = n%10;                      // ones value
+  bin[2] = (n%100 - n%10)/10;         // tens value = tens and ones value minus the ones value
+  bin[1] = (n%1000 - n%100)/100;      // whole number minus the tens and ones values
+  bin[0] = '\0';                      // null terminator
+  // convert all three values into ASCII digits
+
+  static char ascii[5];
+  ascii[0] = (char)(bin[1] + '0');    // ASCII form of binary value
+  ascii[1] = (char)(bin[2] + '0');
+  ascii[2] = (char)(bin[3] + '0');
+  ascii[3] = bin[0]; // null terminator
+
+
+     // return needed value
+     return ascii;
 }
 
 unsigned char ReadNumpad(void)
@@ -262,7 +294,7 @@ unsigned char ReadNumpad(void)
             numpadEntry |= Row4_bin;
         }
     }
-    
+
     WriteDataToLCD(numpadEntry);
     return numpadEntry;
 }
@@ -360,59 +392,266 @@ unsigned char numpadNum (unsigned char numpadEntry)
             return number;
 }
 
+void ColumnsInput()
+{
+    DIRCOL1 = 1;
+    DIRCOL2 = 1;
+    DIRCOL3 = 1;
+}
+void RowsOutput()
+{
+    DIRROW1 = 0;
+    DIRROW2 = 0;
+    DIRROW3 = 0;
+    DIRROW4 = 0;
+}
+
+void ColumnsOutput()
+{
+    DIRCOL1 = 0;
+    DIRCOL2 = 0;
+    DIRCOL3 = 0;
+}
+void RowsInput()
+{
+    DIRROW1 = 1;
+    DIRROW2 = 1;
+    DIRROW3 = 1;
+    DIRROW4 = 1;
+}
+
+int IsValid(char n)
+{
+     int count = 0;
+     for (int i=0; i<8; i++) // Here we want an incremental bitwise shift through the numbers.
+     {
+          if (n%2 == 1)     // this checks to see if the number is odd.  With bitwise shifting each cycle, we should see every bit.
+          {
+               count++;          // increments count, signifying that there is a 1 in that bit space.
+          }
+          n<<1;               // Bitwise shift to left.
+                              // Not sure that this syntax is correct.  Verify.
+     }
+
+     if (count == 3)
+     {
+          return 1;          // Bit 7 and 2 bits must be set from row and column.
+     }
+     else
+     {
+          return 0;
+     }
+}
+
+char NumAssign(char NumFlag)
+{
+
+          char ColMask = 0x10001111;     // 1 = don't care, 0 = check this value
+          char RowMask = 0x11110000;
+
+          char Col1Check = 0x10011111;
+          char Col2Check = 0x10101111;
+          char Col3Check = 0x11001111;
+
+          char Row1Check = 0x11110001;
+          char Row2Check = 0x11110010;
+          char Row3Check = 0x11110100;
+          char Row4Check = 0x11111000;
+
+          char CheckCol = NumFlag | ColMask;
+          char CheckRow = NumFlag | RowMask;
+
+          char number = '$';
+          
+//  Assign number if Col1 is detected
+if (CheckCol == Col1Check)
+{
+     if (CheckRow == Row1Check)
+     {
+          number = '1';
+     }
+     else if (CheckRow == Row2Check)
+     {
+          number = '4';
+     }
+     else if (CheckRow == Row3Check)
+     {
+          number = '7';
+     }
+     else if (CheckRow == Row4Check)
+     {
+          number = '*';
+     }
+}
+
+//  Assign number if Col2 is detected
+else if (CheckCol == Col2Check)
+{
+    if (CheckRow == Row1Check)
+    {
+         number = '2';
+    }
+    else if (CheckRow == Row2Check)
+    {
+         number = '5';
+    }
+    else if (CheckRow == Row3Check)
+    {
+         number = '8';
+    }
+    else if (CheckRow == Row4Check)
+    {
+         number = '0';
+    }
+}
+
+//  Assign number if Col3 is detected
+else if (CheckCol == Col3Check)
+{
+    if (CheckRow == Row1Check)
+    {
+         number = '3';
+    }
+    else if (CheckRow == Row2Check)
+    {
+         number = '6';
+    }
+    else if (CheckRow == Row3Check)
+    {
+         number = '9';
+    }
+    else if (CheckRow == Row4Check)
+    {
+         number = '#';
+    }
+}
+
+//  End of the number assignment
+          
+}
 
 void main(void)
 {
-    // Stage 1: Set I/O on the ports
-    Col1_dir = 0;
-    Col2_dir = 0;
-    Col3_dir = 0;
 
+    ANSEL = 0; // all ports digital
+    ANSELH = 0; // all ports digital
+    CM1CON0 = 0; //comparators off
+    CM2CON0 = 0;//comparators off
+    ADCON0 = 0; //ADC off
+    ADCON1 = 0; //ADC off
+    WPUB = 1; //pullup
+    IOCB = 0; //interrupt on change
 
-    Row1_dir = 1;
-    Row2_dir = 1;
-    Row3_dir = 1;
-    Row4_dir = 1;
-
-    ANSEL = 0;
-
-
-unsigned char numpadEntry = 0x00;
-unsigned char numpadDat = '!';
-
-
-//      LCD prompt
-
-        ClearLCDScreen();
         InitLCD();
         ClearLCDScreen();
         InitLCD();
-
-        // Written strings must be ordered by lines in 16 character segments
-        // Line 5 is only 8 bits long and is not seen by the user
-        WriteStringToLCD("Please enter the");       // Line 1
-        WriteStringToLCD(" and press # for");       // Line 3
-        WriteStringToLCD("        ");               // Line 5 (unseen)
-        WriteStringToLCD("patient's weight");       // Line 2
-        WriteStringToLCD("enter * to erase");       // Line 4
-        delay(50000);
         ClearLCDScreen();
 
-        WriteStringToLCD("Patient Weight:");
+    while(1)
+    {
+    
+    char number[] = "$";
+    // ASCII value of the character entered will be stored here.
+    // Invalid character so that program can't flow without an actual input.
 
-        while(numpadDat == '!')
+    char NumFlag = 0x00000000;
+    // Bit 7 signals whether or not any switch has
+    // been pressed for the cycle.
+    // 1 = switch pressed, 0 = no switch pressed.
+    // Bits 6-4 are columns, bits 3-0 are rows.
+    // 1 = Column or row detected, 0 = No column or row detected.
+
+    int NumFlagText0 = IntToText(NumFlag);    // Verify this is actually 0
+
+    //Columns are outputs, rows are inputs
+    ColumnsOutput();
+    RowsInput();
+    
+    //Turn on Columns
+    COL1 = 1;
+    COL2 = 1;
+    COL3 = 1;
+
+    //Default Rows to 0
+    ROW1 = 0;
+    ROW2 = 0;
+    ROW3 = 0;
+    ROW4 = 0;
+
+    delay(100000); // Delay to give time for MCU to update
+
+        if (ROW1 == 1)
         {
-    // Stage 2: Cycle through the Ports.
-
-        numpadEntry = ReadNumpad();
-        WriteDataToLCD(numpadEntry);
-
-    // Stage 3: Check if entry is valid and assign ASCII data value
- 
-        numpadDat = numpadNum(numpadEntry);
-
+               NumFlag |= 0x10000001;
+               // OR operator used because we don't want
+               // to disturb any earlier edits to the flag.
         }
-        WriteDataToLCD(numpadDat);
+        if (ROW2 == 1)
+        {
+               NumFlag |= 0x10000010;
+        }
+        if (ROW3 == 1)
+        {
+               NumFlag |= 0x10000100;
+        }
+        if (ROW4 == 1)
+        {
+               NumFlag |= 0x10001000;
+        }
+
+    int NumFlagText1 = IntToText(NumFlag);
+
+    
+    //Columns are inputs, rows are outputs
+    ColumnsInput();
+    RowsOutput();
+
+     //Turn on Rows
+     ROW1 = 1;
+     ROW2 = 1;
+     ROW3 = 1;
+     ROW4 = 1;
+
+     //Default Columns to 0
+     COL1 = 0;
+     COL2 = 0;
+     COL3 = 0;
+
+     delay(100000); // Delay to give time for MCU to update
+     
+        if (COL1 == 1)
+        {
+               NumFlag |= 0x10010000;
+        }
+        if (COL2 == 1)
+        {
+               NumFlag |= 0x10100000;
+        }
+        if (COL3 == 1)
+        {
+               NumFlag |= 0x11000000;
+        }
+
+     int NumFlagText2 = IntToText(NumFlag);
+
+
+     // int validFlag = IsValid(NumFlag);
+
+     //if(validFlag == 1)     // only runs if the NumFlag is valid.
+     //{
+        number[0] = NumAssign(NumFlag);
+     //}
+        ClearLCDScreen();
+        WriteStringToLCD(NumFlagText0);     // Initial value
+        WriteStringToLCD(" ");
+        WriteStringToLCD(NumFlagText1);     // After Rows checked
+        WriteStringToLCD(" ");
+        WriteStringToLCD(NumFlagText2);     // After Cols checked
+        WriteStringToLCD(" ");
+        WriteStringToLCD(number);           // ASCII value of number
+     
+    }
+
 
 
     while(1);
