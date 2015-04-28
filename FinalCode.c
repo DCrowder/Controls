32565c39ebe71d166ab64b88a42521a8dd191ced
DCrowder <DCrowder@harding.edu>
2015-04-28 11:09:12 -0500

    Comments and more organized IntToText()
    IntToText() was modified so that it is more intuitive to read.  Most of the code is now commented to help another person understand what is going on.

--- FinalCode.c
+++ FinalCode.c
@@ -1,45 +1,43 @@
 //      Author:     David Crowder
-//      Name:       IncompleteColumnCheck.c
-//      Purpose:    Checks the columns of the number pad and displays the value
-//                  to the screen.
+//      Name:       FinalCode.c
+//      Purpose:    
+
 
 //      MCU header info
-
 #include <PIC16f887.h>
 #include <htc.h>
 #include <pic.h>
 #include <stdio.h>
 #include <math.h>
 
-//      Configuration Bits
+// Configuration Bits
 __CONFIG(FOSC_INTRC_NOCLKOUT & WDTE_OFF & PWRTE_ON & MCLRE_OFF & CP_OFF & CPD_OFF & BOREN_OFF & IESO_OFF & FCMEN_OFF & LVP_OFF);
 __CONFIG(BOR4V_BOR21V & WRT_OFF);
 
-//      LCD Ports
+//.................LCD Information...................../
+
+// LCD Ports
 #define LCD_E   RC5
 #define LCD_RW  RC6
 #define LCD_RS  RC7
 #define LCD_DAT PORTD
 
-//      LCD Directions
+// LCD Directions
 #define LCD_E_Dir   TRISC5
 #define LCD_RW_Dir  TRISC6
 #define LCD_RS_Dir  TRISC7
 #define LCD_DAT_Dir TRISD
 
-//      Delay time for Enable pin: defined by the LCD module datasheet.
+// Delay time for Enable pin: defined by the LCD module datasheet.
 #define EDelay 304 //304 cycles = 38 usec
 
-#define ROWS = 4;
-#define COLS = 4;
+//.............End of LCD Information............./
 
 // ADC Module Information
-
 #define PORT_SEN_DIR TRISA0
 #define ANALOG_SEN ANS0
 #define SENSORH ADRESH
 #define SENSORL ADRESL
-
 
 void delay(unsigned int time)
 {
@@ -58,7 +56,6 @@ void ToggleEpinOfLCD(void)
 	LCD_E = 0;                // data from data bus
 	delay(EDelay);
 }
-
 
 void WriteCommandToLCD(unsigned char Command)
 {
@@ -82,7 +79,7 @@ void InitLCD(void)
 {
     //  Initializes the LCD when the MCU program first runs
 
-	// Firstly make all pins output
+    // Firstly make all pins output
 	LCD_E   	     = 0;      // E = 0
         LCD_RW               = 0;
 	LCD_RS    	     = 0;      // D = 0
@@ -104,7 +101,7 @@ void InitLCD(void)
    WriteCommandToLCD(0x30);
 
    delay(120000);                   // 15 msec
-  /////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////
 
    WriteCommandToLCD(0x38);          //function set
    WriteCommandToLCD(0x0C);          //display on/off control
@@ -113,8 +110,6 @@ void InitLCD(void)
    WriteCommandToLCD(0x06);          //entry mode, set increment
 }
 
-
-
 void WriteStringToLCD(const char *s)
 {
     while(*s)
@@ -122,8 +117,6 @@ void WriteStringToLCD(const char *s)
 	WriteDataToLCD(*s++);   // print first character on LCD
     }
 }
-
-
 
 void ClearLCDScreen(void)
 {
@@ -144,44 +137,52 @@ void ClearLCDScreen(void)
 #define ROW4 RB3
 
 // Direction of Numpad Ports
+
+// Column directions
 #define DIRCOL1 TRISE0
 #define DIRCOL2 TRISE1
 #define DIRCOL3 TRISE2
 
+// Row directions
 #define DIRROW1 TRISB0
 #define DIRROW2 TRISB1
 #define DIRROW3 TRISB2
 #define DIRROW4 TRISB3
 
-char hexDigit(int n)
-{
-      return n + '0';
-}
-
 char * IntToText(int n)
 {
-  static int bin[5];
-  bin[4] = n%10;                      // ones value
-  bin[3] = (n%100 - n%10)/10;         // tens value = tens and ones value minus the ones value
-  bin[2] = (n%1000 - n%100)/100;      // whole number minus the tens and ones values
-  bin[1] = (n - n%1000)/1000;
-  bin[0] = '\0';                      // null terminator
-  // convert all three values into ASCII digits
+    // IntToText(n) reads an integer and creates a 5 character array
+    // that corresponds to a four digit ASCII format of the integer.
 
-  static char ascii[5];
-  ascii[0] = (char)(bin[1] + '0');    // ASCII form of binary value
-  ascii[1] = (char)(bin[2] + '0');
-  ascii[2] = (char)(bin[3] + '0');
-  ascii[3] = (char)(bin[4] + '0');
-  ascii[4] = bin[0]; // null terminator
+    // Modulus (%) takes the integer n and can find the remainder
+    // of n divided by 10, 100, and 1000.  This is used to find
+    // the single digit integer of n for the thousands, hundreds, tens,
+    // and ones place.
+
+    static int bin[5];
+    bin[0] = (n - n%1000)/1000;         // thousands value
+    bin[1] = (n%1000 - n%100)/100;      // hundreds value
+    bin[2] = (n%100 - n%10)/10;         // tens value = tens and ones value minus the ones value
+    bin[3] = n%10;                      // ones value
+    bin[4] = '\0';                      // null terminator
+    
+    // convert the four values into ASCII digits
+    // must be typecasted because the array is converted from int to char.
+    static char ascii[5];
+    ascii[0] = (char)(bin[0] + '0');    // ASCII form of binary value
+    ascii[1] = (char)(bin[1] + '0');    // swapping order so that
+    ascii[2] = (char)(bin[2] + '0');
+    ascii[3] = (char)(bin[3] + '0');
+    ascii[4] = bin[4]; // null terminator
 
 
-     // return needed value
-     return ascii;
+  // return 4 digit ASCII array of integer n with closing null terminator
+  return ascii;
 }
 
 void ColumnsOutput()
 {
+    // Set direction of column to an output = 0
     DIRCOL1 = 0;
     DIRCOL2 = 0;
     DIRCOL3 = 0;
@@ -189,6 +190,7 @@ void ColumnsOutput()
 
 void RowsInput()
 {
+    // Set direction of row to an input = 1
     DIRROW1 = 1;
     DIRROW2 = 1;
     DIRROW3 = 1;
@@ -197,12 +199,14 @@ void RowsInput()
 
 void ColumnsInput()
 {
+    // Set direction of column to an input = 1
     DIRCOL1 = 1;
     DIRCOL2 = 1;
     DIRCOL3 = 1;
 }
 void RowsOutput()
 {
+    // Set direction of row to an output = 0
     DIRROW1 = 0;
     DIRROW2 = 0;
     DIRROW3 = 0;
@@ -211,6 +215,16 @@ void RowsOutput()
 
 char assignNumpad()
 {
+    // assignNumpad() reads the ports corresponding to the rows and columns
+    // of the number pad and stores it in memory.
+
+
+    // keys is an array with an additional row so '$' can be stored.
+    // '$' is the default character used because $ should never display to
+    // the screen.
+    // The rest of the values are stored so the key value can be pulled
+    // when a button is pressed.
+    
     unsigned char keys[5][3] =
     {'1','2','3',
     '4','5','6',
@@ -218,29 +232,31 @@ char assignNumpad()
     '*','0','#',
     '$','\0','\0'};
 
+    // Rows 1-4 have flags to store the current row values
     int R1Flag = 0;
     int R2Flag = 0;
     int R3Flag = 0;
     int R4Flag = 0;
 
+    // Columns 1-3 have flags to store the current column values
     int C1Flag = 0;
     int C2Flag = 0;
     int C3Flag = 0;
 
     
-
+    // Default invalid character stored as char called digit
     char digit = '$';
 
-    //Columns are outputs, rows are inputs
+    // Columns are outputs, rows are inputs
     ColumnsOutput();
     RowsInput();
 
-    //Turn on Columns
+    // Turn on columns so that the rows will go high when a button is pressed
     COL1 = 1;
     COL2 = 1;
     COL3 = 1;
 
-
+    // Assign current row values to each flag
     R1Flag = ROW1;
     R2Flag = ROW2;
     R3Flag = ROW3;
@@ -250,21 +266,24 @@ char assignNumpad()
     ColumnsInput();
     RowsOutput();
 
-    //Turn on rows
+    //Turn on rows so that the columns will go high when a button is pressed
     ROW1 = 1;
     ROW2 = 1;
     ROW3 = 1;
     ROW4 = 1;
 
+    // Assign current column values to each flag
     C1Flag = COL1;
     C2Flag = COL2;
     C3Flag = COL3;
 
-    int RowFlag = 4;    //invalid array number: $
+    // Invalid array number: $ by default.  Will be changed later.
+    int RowFlag = 4;
     int ColFlag = 0;
 
 
-    // Sweep Rows
+    // Sweep Rows by checking if a flag is HIGH.
+    // If a flag is high, the RowFlag is assigned with the row number detected.
     if (R1Flag)
     {
            RowFlag = 0;
@@ -282,7 +301,8 @@ char assignNumpad()
            RowFlag = 3;
     }
 
-    // Sweep Columns
+    // Sweep Columns by checking if a flag is HIGH.
+    // If a flag is high, the ColFlag is assigned with the column number detected.
     if (C1Flag)
     {
            ColFlag = 0;
@@ -296,8 +316,10 @@ char assignNumpad()
            ColFlag = 2;
     }
 
+    // Assign the char digit with the ASCII values found in the keys array.
     digit = keys[RowFlag][ColFlag];
 
+    // Return the assigned char
     return digit;
 
 
@@ -305,22 +327,56 @@ char assignNumpad()
 
 void InitADC(void)
 {
+    // All information for ADC initialization can be found on
+    // page 100 of the PIC16F887 datasheet.
+
     PORT_SEN_DIR = 1;       // Sets the sensor port as an input
     ANALOG_SEN = 1;         // Sets the sensor pin as analog
-    ADCON1 = 0xFF;          // left justified, Vref pins active
-    ADCON0 = 0x01;
-    delay(EDelay);             // Delay the required length of acquisition (Fosc/2)
-    ADCON0 = 0x03;          // Sets the GO/!DONE bit so that ADC begins
+    
+    // A/D Control Registers
+    ADCON0 = 0x01;          // Analog channel 1 (AN0) ADCON0<5:2> = CHS<3:0> 0000
+                            // Fosc/2 as conversion clock bits ADCON0<7:6> = ADCS<2:0> = 00
+                            // ADC Enable bit turned on  ADCON0<0> = ADON = 0 (p.104)
+    
+    ADCON1 = 0xFF;          // left justified, Vref pins active (p.105)
 
+    delay(EDelay);          // Delay the required length of acquisition (Fosc/2)
+                            // EDelay is close enough to the necessary delay value
+                            // and does not hinder the program.
+                            // The sensor has been tested successfully with this delay.
+    
+    ADCON0 = 0x03;          // Sets the GO/!DONE bit so that ADC begins (p. 105)
 
     while(ADCON0 == 0x03);  // Poll for GO/!DONE bit to be cleared
 }
 
 void PromptLCD(int stage)
 {
-    ClearLCDScreen();
-    
-    if (stage == 1)
+    ClearLCDScreen();       // Clear the screen before any stage messages
+
+    //  The LCD displays incorrectly without sending commands.
+    //  Each line is labeled by order (1-5).
+    //  Each line is 16 characters long except for line 5.
+    //  Line 5 has 8 characters that do not display to the LCD screen.
+
+    //  The lines display in 1, 3, 5, 2, and 4.
+    //  Imagine the display reads A-Z, 0-9, and then repeats.
+
+    //-----------Sample LCD-------------//
+    //      ABCDEFGHIJKLMNOP    Line 1  //
+    //      EFGHIJKLMNOPQRST    Line 2  //
+    //      QRSTUVWXYZ012345    Line 3  //
+    //      UVWXYZ0123456789    Line 4  //
+    //      6789ABCD            Line 5  //
+    //----------------------------------//
+
+
+    // Check patient's
+    // leg & footplate
+    // straps.  Press
+    // # to continue."
+
+    if (stage == 1)     // Check patient
     {
         WriteStringToLCD(" Check patient's");       // 1st line
         WriteStringToLCD(" straps.  Press ");       // 3rd line
@@ -330,7 +386,12 @@ void PromptLCD(int stage)
 
     }
 
-    if (stage == 2)
+    // Check ratchets:
+    // Motor side: down
+    // Other side: up
+    // # to continue.
+
+    if (stage == 2)     // Check ratchet
     {
         WriteStringToLCD(" Check ratchets:");       // 1st line
         WriteStringToLCD("Other side: up  ");       // 3rd line
@@ -339,7 +400,14 @@ void PromptLCD(int stage)
         WriteStringToLCD(" # to continue. ");       // 4th line
     }
 
-    if (stage == 3)
+    // Enter Patient
+    // Weight: _                      (blinking cursor)
+    // Enter: # Back *
+
+    // The "Weight: " string is last, therefore in code we can display
+    // the weight without sending an additional command.
+
+    if (stage == 3)     // Enter patient weight
     {
         WriteStringToLCD(" Enter  Patient ");       // 1st line
         WriteStringToLCD("Enter: # Back: *");       // 3rd line
@@ -435,8 +503,8 @@ void main(void)
                     }
                     else if (digit[0] != '#' && count < 3 && digit[0] != '*')   // only if operator doesn't want to continue
                     {
-                        number[count] = digit[0];   //place the number into the code
-                        WriteStringToLCD(digit);
+                        number[count] = digit[0];   // place the digit into the number array
+                        WriteStringToLCD(digit);    // display 
                         count++;
                     }
                     delay(1000);       // Delay after action has been done
@@ -489,7 +557,24 @@ void main(void)
         char UserForceOne[2] = "0";
         char UserForceTen[2] = "0";
 
+        float thresholdTen = threshold*10;
+        float forceMaxTenFloat = (thresholdTen - 10*b)/a;
+        char forceMaxTenChar = (char) forceMaxTenFloat;
+
+        char * UserForceMax = IntToText(forceMaxTenChar);
+
+        char UserForceMaxDec[2] = "0";
+        char UserForceMaxOne[2] = "0";
+        char UserForceMaxTen[2] = "0";
+
+
+        UserForceMaxDec[0] = forceMaxTenChar%10 + '0';  // quick fix to decimal not displaying right
+        UserForceMaxOne[0] = UserForceMax[2];
+        UserForceMaxTen[0] = UserForceMax[1];
+
         ClearLCDScreen();       // clear screen for next stage
+
+
 
         while(1)
         {
@@ -531,11 +616,9 @@ void main(void)
 
             //  IntToText converts an integer to ASCII
 
-            float force = (SENSOR - b)/a;
-//            int forceInt = force;
-            float forceTenFloat = force*10;
-            char forceTenChar = (char) forceTenFloat*10;
-            int forceTenInt = (int) forceTenFloat;
+            float sensorTen = SENSOR*10;
+            float forceTenFloat = (sensorTen - 10*b)/a;
+            char forceTenChar = (char) forceTenFloat;
 
             if (displaySensor%50 == 0)
             {
@@ -552,24 +635,18 @@ void main(void)
 
                 if (SENSOR >= 435)
                 {
-                    int * UserForce = IntToText(forceTenInt);
+                    char * UserForce = IntToText(forceTenFloat);
 
-                    UserForceDec[0] = (forceTenChar)%10 + '0';  // quick fix to decimal not displaying right
-                    UserForceOne[0] = UserForce[1];
-                    UserForceTen[0] = UserForce[2];
-
-                    
+                    UserForceDec[0] = forceTenChar%10 + '0';  // quick fix to decimal not displaying right
+                    UserForceOne[0] = UserForce[2];
+                    UserForceTen[0] = UserForce[1];
 
                     WriteStringToLCD(UserForceTen);
+
                     WriteStringToLCD(UserForceOne);
                     WriteStringToLCD(".");       // currently can't get proper value
                     WriteStringToLCD(UserForceDec);
                     WriteStringToLCD("  ");
-
-                    if (force < 10)
-                    {
-                        WriteStringToLCD(" ");
-                    }
 //
                 }                
                 else
@@ -589,28 +666,32 @@ void main(void)
 
 
                 // line 4: --####----####--
-                int * UserThreshold = IntToText(threshold); // threshold text
+                char * UserThreshold = IntToText(threshold); // threshold text
                 WriteStringToLCD("  ");
                 WriteStringToLCD(UserThreshold);    // display threshold value text
                 WriteStringToLCD("    ");
-                WriteStringToLCD(IntToText(forceMax));
+
+                // Example: 15.0
+                if (UserForceMaxTen[0] == '1')
+                {
+                    WriteStringToLCD(UserForceMaxTen);
+                }
+                else
+                {
+                    WriteStringToLCD(" ");
+                }
+                WriteStringToLCD(UserForceMaxOne);
+                WriteStringToLCD(".");       // currently can't get proper value
+                WriteStringToLCD(UserForceMaxDec);
+                
                 WriteStringToLCD("  ");
                 // end of line 4
-
-                
-                
 
             }
 
             displaySensor++;
 
         while(1);
-        // Written strings must be ordered by lines in 16 character segment
+        
 
         }
-            
-
-
-
-
-
